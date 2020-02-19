8f2a1b29685c2c4fad5bfba170627ff961e5e7f3
13952522076 <xuma@my.unt.edu>
2020-02-19 12:21:35 -0600

    1

--- validate/test_4.py
+++ validate/test_4.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch.nn.functional as F
 import math
 
+
 # A =torch.rand(1,10,10)
 # B = torch.cat((2*A,A),dim=0)
 # print(B.shape)
@@ -15,16 +16,17 @@ import math
 # print(B.var())
 
 
-path = "/Users/melody/Downloads/model_best.pth.tar"
+path = "/Users/melody/Downloads/epoch63checkpoint.pth.tar"
 check_point = torch.load(path,map_location='cpu')
 
 # print(check_point['best_prec1'])
 # exit()
 
 for k, v in check_point['state_dict'].items():
-    if "distribution_scale" in k:
+    if "distribution_std" in k:
         # print("{} : {}".format(k,v))
-        print(v.data)
+        # print(v.data)
+        print(abs(v).min())
     # if "distribution_var" in k:
     #     print("{} : {}".format(k,v))
     # if "normal_loc" in k:
--- validate/test_7.py
+++ validate/test_7.py
@@ -8,7 +8,24 @@ import numpy as np
 import torch.nn.functional as F
 import math
 
-a = torch.rand(3)
-b = a.mean()
-print(a.var())
-print((-(a-b)).var())
+path = "/Users/melody/Downloads/epoch63checkpoint.pth.tar"
+check_point = torch.load(path,map_location='cpu')
+
+# print(check_point['best_prec1'])
+# exit()
+
+def _get_mask(k):
+    # assume square and odd number, because lazy.
+    y = torch.arange(0, k) - k // 2 + 0.0
+    y1 = y.reshape(k, 1).expand((k, k))
+    y2 = y.reshape(1, k).expand((k, k))
+    mask = torch.sqrt(y1.pow(2) + y2.pow(2))
+    return mask.unsqueeze(dim=-1).unsqueeze(dim=-1)
+
+mask = _get_mask(3)
+
+for k, v in check_point['state_dict'].items():
+    if k.str.endswith(""):
+        print(k)
+        y = -(1.0 / (k.distribution_std * math.sqrt(2 * math.pi))) \
+            * torch.exp(-((mask * k.distribution_zoom) ** 2) / (2 * (k.distribution_std ** 2)))
