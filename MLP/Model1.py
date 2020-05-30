a9e843928ffcfd3b7d52f05eae7da7a070b10ba7
13952522076 <xuma@my.unt.edu>
2020-05-30 18:12:01 -0500

    update model

--- MLP/Model1.py
+++ MLP/Model1.py
@@ -12,11 +12,9 @@ class Model1(nn.Module):
     def __init__(self):
         super(Model1, self).__init__()
         self.fc1 = nn.Linear(784, 784)
-        self.fc2 = nn.Linear(784, 1568)
-        self.fc3 = nn.Linear(1568, 784)
-        self.fc4 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(784, 128)
         self.dropout = nn.Dropout2d(0.5)
-        self.fc5 = nn.Linear(128, 10)
+        self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = torch.flatten(x, 1)
@@ -24,12 +22,8 @@ class Model1(nn.Module):
         x = F.relu(x)
         x = self.fc2(x)
         x = F.relu(x)
-        x = self.fc3(x)
-        x = F.relu(x)
-        x = self.fc4(x)
-        x = F.relu(x)
         x = self.dropout(x)
-        x = self.fc5(x)
+        x = self.fc3(x)
         output = F.log_softmax(x, dim=1)
         return output
 
