02f5a1db0db87a36ae31d23b108598dcb0951b2a
Xu Ma <xuma@my.unt.edu>
2021-09-06 12:02:32 -0400

    Update main.py

--- Loss_landscape/main.py
+++ Loss_landscape/main.py
@@ -250,7 +250,7 @@ def validate(val_loader, model, criterion, args):
 
             if i % args.print_freq == 0:
                 progress.display(i)
-                print(f"loss is {losses}")
+                print(f"loss is {losses.avg}")
 
         # TODO: this should also be done with the ProgressMeter
         print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
