157dccd87b80edeb677e7ddb2959101fd8ca6606
Xu Ma <xuma@my.unt.edu>
2021-09-06 22:36:17 -0400

    Update question.py

--- Loss_landscape/question.py
+++ Loss_landscape/question.py
@@ -225,7 +225,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
     for w1 in list_1:
         for w2 in list_2:
-            w1,w2 = 0,0
+            w1,w2 = -0.5,0.0
             print("\n\n===> w1{w1:.1f} w2{w2:.1f}".format(w1=w1, w2=w2))
             combined_weights = get_combined_weights(direction1, direction2, checkpoint, w1,w2)
             model.load_state_dict(combined_weights)
