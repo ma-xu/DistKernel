3c10b68e2f223de0040688ab8e1957f7d023cae9
Xu Ma <xuma@my.unt.edu>
2021-09-07 02:52:06 -0400

    Update load_pretrained.py

--- Loss_landscape/load_pretrained.py
+++ Loss_landscape/load_pretrained.py
@@ -93,8 +93,9 @@ def rand_normalize_directions(args, states, ignore='biasbn'):
 def get_combined_weights(direction1, direction2, pretrained, weight1, weight2, weight_pretrained=1.0):
     new_dict = OrderedDict()
     for (k, d1),(_,d2), (_,w) in zip(direction1.items(), direction2.items(), pretrained.items()):
-        new_dict[k] = (weight1 * d1 + weight2 * d2 + weight_pretrained * w)
-        # new_dict[k] = (weight1 * d1 + weight2 * d2 + weight_pretrained * w)/(weight1+weight2+weight_pretrained)
+        # new_dict[k] = (weight1 * d1 + weight2 * d2 + weight_pretrained * w)
+        new_dict[k] = (weight1 * d1 + weight2 * d2 + weight_pretrained * w)/\
+                      (abs(weight1)+abs(weight2)+abs(weight_pretrained))
     return new_dict
 
 
